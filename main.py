1451ae9ed085df2912a1a6462a627db9d23fd13e
samuelsantosro <samuelsantosro@hotmail.com>
2025-06-23 23:57:14 -0300

    commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#pip install fastapi uvicorn sqlalchemy passlib[bcrypt] python-jose[cryptography] python-dotenv python-multipart
+
+#uvicorn main:app --reload
+
+from fastapi import FastAPI
+
+app = FastAPI()
